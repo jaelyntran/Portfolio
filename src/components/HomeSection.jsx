415affc12b4c75d2b64dca6fbe8f682086db4927
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2025-10-25 01:16:05 -0700

    Update not found page

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { ArrowDown } from "lucide-react"
+import { ArrowDown } from "lucide-react";
 
 export const HomeSection = () => {
     return <section id="home"
@@ -28,7 +28,7 @@ export const HomeSection = () => {
                     <div className={cn("absolute bottom-8 left-1/2 transform -translate-x-1/2",
                                        "flex flex-col items-center animate-bounce")}>
                         <span className="text-sm text-muted-foreground mb-2">Scroll</span>
-                        <ArrowDown className="h-5 v-5 text-primary" />
+                        <ArrowDown className="h-5 w-5 text-primary" />
                     </div>
            </section>
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,9 @@
+import { Frown } from "lucide-react";
+
 export const NotFound = () => {
-    return <div>Not Found</div>;
+    return <div className="relative min-h-screen flex flex-col items-center justify-center px-10">
+                <Frown className="h-40 w-40 text-muted-foreground mb-5 opacity-0 animate-fade-in"/>
+                <span className="text-4xl md:text-6xl font-bold text-center opacity-0 animate-fade-in-delay-1">404</span>
+                <span className="text-base md:text-lg font-semibold text-center opacity-0 animate-fade-in-delay-2">Page Not Found</span>
+           </div>;
 };
