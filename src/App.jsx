2f6e78a9abc762f3aa55c7a0d3fa9506969a162f
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2025-10-30 03:55:13 -0700

    Adjusted configuration for vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
 function App() {
 
-    return (
-        <HashRouter>
+  return (
+    <>
+        <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
-        </HashRouter>
-    );
+        </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { ArrowDown } from "lucide-react";
 
 export const HomeSection = () => {
-    return <section id="home"
+    return <section id="#home"
                     className="relative min-h-screen flex flex-col items-center justify-center px-4">
                     <div className="container max-w-4xl mx-auto text-center z-10">
                         <div className="space-y-6">
