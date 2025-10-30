229ed530204691cfa3b5075bbd3f9c70702bc35c
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2025-10-30 03:32:01 -0700

    Adjust overlay issue and navbar background when scrolled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import { AboutSection } from "./components/AboutSection";
-import { SkillsSection } from "./components/SkillsSection";
-import { ProjectsSection } from "./components/ProjectsSection";
-import { ContactSection } from "./components/ContactSection";
 
 function App() {
 
@@ -12,10 +8,6 @@ function App() {
         <HashRouter>
             <Routes>
                 <Route index element={<Home />} />
-                <Route path="/about" element={<AboutSection />} />
-                <Route path="/skills" element={<SkillsSection />} />
-                <Route path="/projects" element={<ProjectsSection />} />
-                <Route path="/contact" element={<ContactSection />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </HashRouter>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ export const NavBar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.screenY > 10)
+            setIsScrolled(window.scrollY > 10)
         }
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
@@ -25,42 +25,46 @@ export const NavBar = () => {
 
 
     return (
-        <nav className={cn("fixed w-full z-40 transition-all duration-300",
-                           isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5")}>
-            <div className="container flex items-center justify-between">
-                <a className="text-xl font-bold text-primary flex items-center" href="#home">
-                    <span className="relative z-10">
-                        <span className="text-glow text-foreground">Jaelyn Tran</span> Portfolio
-                    </span>
-                </a>
+        <>
+            <nav className={cn("fixed w-full z-40 transition-all duration-300",
+                               isScrolled || isMenuOpen
+                               ? "py-3 bg-background/80 backdrop-blur-md shadow-xs"
+                               : "py-5",
+                               "z-50")}>
+                <div className="container flex items-center justify-between">
+                    <a className="text-xl font-bold text-primary flex items-center" href="#home">
+                        <span className="relative z-10">
+                            <span className="text-glow text-foreground">Jaelyn Tran</span> Portfolio
+                        </span>
+                    </a>
 
-                {/* desktop */}
-                <div className="hidden md:flex space-x-8">
-                    {navItems.map((item, key) => (
-                        <a key={key} href={item.href}
-                           className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                            {item.name}
-                        </a>))}
-                    <ThemeToggle />
-                </div>
-
-                {/* mobile */}
-                <div className="md:hidden flex flex-col items-end p-2 z-50 transition-colors">
-                    <div>
+                    {/* desktop */}
+                    <div className="hidden md:flex space-x-8">
+                        {navItems.map((item, key) => (
+                            <a key={key} href={item.href}
+                               className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                {item.name}
+                            </a>))}
                         <ThemeToggle />
                     </div>
-                    <button onClick={() => setIsMenuOpen((prev) => !prev)}
-                            className="md:hidden text-foreground p-2"
-                            aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
-                        {" "}
-                        {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}
-                    </button>
-                </div>
 
-                <div className={cn("fixed inset-0 background/95 backdrop-blur-md z-40",
-                                  "flex flex-col items-center justify-center",
-                                  "transition-all duration-300 md-hidden",
-                                  isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")}>
+                    {/* mobile */}
+                    <div className="md:hidden flex flex-col items-end p-2 z-50 transition-colors">
+                        <div>
+                            <ThemeToggle />
+                        </div>
+                        <button onClick={() => setIsMenuOpen((prev) => !prev)}
+                                className="md:hidden text-foreground p-2"
+                                aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
+                            {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}
+                        </button>
+                    </div>
+                </div>
+            </nav>
+
+            {isMenuOpen && (
+                <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40",
+                                   "flex flex-col items-center justify-center transition-all duration-300 md:hidden")}>
                     <div className="flex flex-col space-y-8 text-xl">
                         {navItems.map((item, key) => (
                             <a key={key} href={item.href}
@@ -70,6 +74,7 @@ export const NavBar = () => {
                             </a>))}
                     </div>
                 </div>
-            </div>
-        </nav>);
-}
+            )}
+        </>
+    );
+};
